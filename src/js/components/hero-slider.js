46b12f27d3c8b7aadd98a0fb035afc6d5c8eb0d1
BAO <andriy.bezkorovajnyj@gmail.com>
2021-11-08 14:26:43 +0200

    banner done

---
+++ src/js/components/hero-slider.js
@@ -0,0 +1,14 @@
+
+import Swiper from '../vendor/swiper-bundle.js';
+
+  let swiper = new Swiper(".swiper__hero", {
+   
+ scrollbar: {
+  el: ".swiper-scrollbar",
+   hide: false,
+  }, 
+
+});
+
+
+
